72ec8bee16d6aa41ce02a62c29ee5f2c53c13104
Matt Mikuta <mmikuta@hawk.iit.edu>
2016-10-31 01:18:37 -0500

    Edited types.h

--- minix/servers/mcast/types.h
+++ minix/servers/mcast/types.h
@@ -1,4 +1,5 @@
 #define MAX_GROUPS 32
+#define MAX_PROCS 100
 int ProcessList[MAX_PROCS];
 
 typedef struct {
@@ -10,12 +11,14 @@ typedef struct {
 	int datasize;
 } mc_member_t;
 
+typedef mc_member_t *member_list_t[MAX_PROCS];
+
 typedef struct {
 	//group number implied as index in list
 	int nmembers;
-	mc_member_t[MAX_PROCS];
+	member_list_t member_list;
 } mc_group_t;
 
-typedef mc_group_t[MAX_GROUPS] mc_group_list_t;
-
- 
+typedef mc_group_t group_list_t[MAX_GROUPS];
+//mc_group_t mc_group_list_t[MAX_GROUPS];
+group_list_t group_list;
