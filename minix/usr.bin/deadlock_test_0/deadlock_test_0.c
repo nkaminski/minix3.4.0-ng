0a72eb87e916b68603c3f17596beccdc09c1a533
nashkaminski <nashkaminski@kaminski.io>
2016-11-02 18:55:25 -0400

    deadlock test

---
+++ minix/usr.bin/deadlock_test_0/deadlock_test_0.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+const int group_nr = 0;
+const char msg[] = "SettingTheRightBitsInTheRightPlaces!";
+int main(int argc, char** args) {
+	int rv;
+   rv = openGroup(group_nr);
+	printf("returned %i\n",rv);
+	printf("testing msend syscall to group %d\n",group_nr);
+	rv = msend(group_nr, msg, sizeof(msg));
+	printf("returned %i\n",rv);
+   if(rv != -ELOCKED){
+           printf("Deadlock detection not functional\n");
+   }
+	rv = closeGroup(group_nr);
+	printf("returned %i\n",rv);
+   return 0;
+}
