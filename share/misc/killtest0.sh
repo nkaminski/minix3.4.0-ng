b60a34d78de6d15d3312542fd3ca3b4a247f520b
nashkaminski <nashkaminski@kaminski.io>
2016-11-03 02:26:40 -0400

    rename and tweak kill tests

--- share/misc/killtest0.sh
+++ share/misc/killtest0.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-echo Killing Test 0 starting
+echo Interrupted syscall test 0 starting
 echo 2 receivers in Group 0
-echo 1 receiver calls receive from Group 0
+echo 2 receiver call receive from Group 0
+echo 1 receiver gets killed
 echo Sender sends to Group 0
-echo Sender gets killed
-echo The other receiver calls receive from Group 0
-echo EXPECTED OUTPUT: One copy of the actual message
-kill_test_0 &
-kill_test_1 &
+echo EXPECTED OUTPUT: One copy of message
+kill_test_3 &
+kill_test_4 &
 sleep 1
-kill_test_2 &
-ps | grep -v grep | grep kill_test_2 | awk '{print $1}' | xargs kill
+ps | grep -v grep | grep kill_test_3 | awk '{print $1}' | xargs kill
 sleep 1
-echo One receiver should still be running
 ps | grep -v grep | grep kill_test
-echo --Test script exiting--
+echo One receiver should be running
+kill_test_5
--- share/misc/killtest1.sh
+++ share/misc/killtest1.sh
@@ -2,12 +2,17 @@
 
 echo Killing Test 1 starting
 echo 2 receivers in Group 0
-echo 2 receiver call receive from Group 0
-echo 1 receiver gets killed
+echo 1 receiver calls receive from Group 0
 echo Sender sends to Group 0
-kill_test_3 &
-kill_test_4 &
+echo Sender gets killed
+echo The other receiver calls receive from Group 0
+echo EXPECTED OUTPUT: One copy of the actual message
+kill_test_0 &
+kill_test_1 &
 sleep 1
-ps | grep -v grep | grep kill_test_3 | awk '{print $1}' | xargs kill
+kill_test_2 &
+ps | grep -v grep | grep kill_test_2 | awk '{print $1}' | xargs kill
 sleep 1
-kill_test_5
+echo One receiver should still be running
+ps | grep -v grep | grep kill_test
+echo --Test script exiting--
