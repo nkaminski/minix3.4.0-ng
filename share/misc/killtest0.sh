0b29e84e755bff354cb6f95f4d823be2d7f17c92
nashkaminski <nashkaminski@kaminski.io>
2016-11-03 01:58:52 -0400

    fix interrupted syscall tests

--- share/misc/killtest0.sh
+++ share/misc/killtest0.sh
@@ -6,7 +6,13 @@ echo 1 receiver calls receive from Group 0
 echo Sender sends to Group 0
 echo Sender gets killed
 echo The other receiver calls receive from Group 0
+echo EXPECTED OUTPUT: One copy of the actual message
 kill_test_0 &
 kill_test_1 &
+sleep 1
 kill_test_2 &
 ps | grep -v grep | grep kill_test_2 | awk '{print $1}' | xargs kill
+sleep 1
+echo One receiver should still be running
+ps | grep -v grep | grep kill_test
+echo --Test script exiting--
--- share/misc/killtest1.sh
+++ share/misc/killtest1.sh
@@ -7,5 +7,7 @@ echo 1 receiver gets killed
 echo Sender sends to Group 0
 kill_test_3 &
 kill_test_4 &
-ps | grep -v grep | grep kill_test_2 | awk '{print $1}' | xargs kill
-kill_test_5 &
+sleep 1
+ps | grep -v grep | grep kill_test_3 | awk '{print $1}' | xargs kill
+sleep 1
+kill_test_5
